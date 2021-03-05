b90fd00efe7917f777bbb8af530a25d52643dd2d
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-05 20:35:30 -0300

    feat(server): ensure SignUpController return an error if no name if provider

--- server/src/presentation/controllers/signup.ts
+++ server/src/presentation/controllers/signup.ts
@@ -2,7 +2,8 @@ export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async handle(httpRequest: any): Promise<any> {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: name')
     };
   }
 }
