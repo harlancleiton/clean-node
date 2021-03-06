ba69a00bf1e94feb21cac057d54e49d7f2e11e1b
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-06 01:55:43 -0300

    chore(server): add jest configs and scripts

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,13 +4,17 @@ const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/*-protocols.ts'
+  ],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text-summary', 'lcov'],
+  coverageReporters: ['text-summary', 'text', 'lcov'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>'
   }),
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['__tests__/**/*.ts', '**/?(*.)+(spec|test).ts']
+  testMatch: ['**/?(*.)+(spec|test).ts']
 };
