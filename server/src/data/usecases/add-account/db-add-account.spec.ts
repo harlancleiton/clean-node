3c29cdb2f159cf7516a6f5ff98330cd85f73eb10
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-06 02:49:27 -0300

    test(server): ensure DbAddAccount calls Hasher with correct password

---
+++ server/src/data/usecases/add-account/db-add-account.spec.ts
@@ -0,0 +1,34 @@
+import { Hasher } from '~/data/protocols';
+import { AddAccountModel } from '~/domain/usecases';
+
+import { DbAddAccount } from './db-add-account';
+
+describe('DbAddAccountUsecase', () => {
+  let sut: DbAddAccount;
+  let hasherStub: Hasher;
+
+  beforeEach(() => {
+    class HasherStub implements Hasher {
+      async make(payload: string): Promise<string> {
+        return `hashed_password:${payload}`;
+      }
+    }
+
+    hasherStub = new HasherStub();
+    sut = new DbAddAccount(hasherStub);
+  });
+
+  it('should call Hasher with correct password', async () => {
+    jest.spyOn(hasherStub, 'make');
+
+    const addAccountModel: AddAccountModel = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    };
+
+    await sut.execute(addAccountModel);
+
+    expect(hasherStub.make).toBeCalledWith('valid_password');
+  });
+});
