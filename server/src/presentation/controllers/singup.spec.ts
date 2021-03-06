56d0d1be53ad9f010b4b457dd7c9c2ba0dabec09
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-05 21:20:57 -0300

    test(server): ensure SignUpController return an error if no password confirmation is provided

--- server/src/presentation/controllers/singup.spec.ts
+++ server/src/presentation/controllers/singup.spec.ts
@@ -2,7 +2,7 @@ import { MissingParamException } from '../exceptions';
 import { SignUpController } from './signup';
 
 describe('SignUpController', () => {
-  it('should return 400 if no name is provider', async () => {
+  it('should return 400 if no name is provided', async () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -16,7 +16,7 @@ describe('SignUpController', () => {
     expect(httpResponse.body).toEqual(new MissingParamException('name'));
   });
 
-  it('should return 400 if no email is provider', async () => {
+  it('should return 400 if no email is provided', async () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -30,7 +30,7 @@ describe('SignUpController', () => {
     expect(httpResponse.body).toEqual(new MissingParamException('email'));
   });
 
-  it('should return 400 if no password is provider', async () => {
+  it('should return 400 if no password is provided', async () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -42,5 +42,21 @@ describe('SignUpController', () => {
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamException('password'));
+  });
+
+  it('should return 400 if no password confirmation is provided', async () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamException('passwordConfirmation')
+    );
   });
 });
