8249ade7996f26d0270d6d96786af653b71b4cf0
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-06 03:35:28 -0300

    test(server): ensure DbAddAccount calls AddAccountRepository with correct values

--- server/src/data/usecases/add-account/db-add-account.spec.ts
+++ server/src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,11 +1,23 @@
 import { DbAddAccount } from './db-add-account';
-import { AddAccountModel, Hasher } from './db-add-account-protocols';
+import {
+  AccountModel,
+  AddAccountModel,
+  AddAccountRepository,
+  Hasher
+} from './db-add-account-protocols';
 
 describe('DbAddAccountUsecase', () => {
   let sut: DbAddAccount;
+  let addAccountRepositoryStub: AddAccountRepository;
   let hasherStub: Hasher;
 
   beforeEach(() => {
+    class AddAccountRepositoryStub implements AddAccountRepository {
+      async add(addAccountModel: AddAccountModel): Promise<AccountModel> {
+        return { ...addAccountModel, id: 'id' };
+      }
+    }
+
     class HasherStub implements Hasher {
       async make(payload: string): Promise<string> {
         return `hashed_password:${payload}`;
@@ -13,7 +25,8 @@ describe('DbAddAccountUsecase', () => {
     }
 
     hasherStub = new HasherStub();
-    sut = new DbAddAccount(hasherStub);
+    addAccountRepositoryStub = new AddAccountRepositoryStub();
+    sut = new DbAddAccount(hasherStub, addAccountRepositoryStub);
   });
 
   it('should call Hasher with correct password', async () => {
@@ -44,5 +57,23 @@ describe('DbAddAccountUsecase', () => {
     await expect(sut.execute(addAccountModel)).rejects.toThrow();
 
     expect(hasherStub.make).toBeCalledWith('valid_password');
+  });
+
+  it('should call AddAccountRepository with correct values', async () => {
+    jest.spyOn(addAccountRepositoryStub, 'add');
+
+    const addAccountModel: AddAccountModel = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    };
+
+    await sut.execute(addAccountModel);
+
+    expect(addAccountRepositoryStub.add).toBeCalledWith({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: await hasherStub.make('valid_password')
+    });
   });
 });
