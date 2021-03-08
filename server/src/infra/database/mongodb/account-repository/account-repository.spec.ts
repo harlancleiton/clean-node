9df7f0cb995c7e1f02c384f7b2fca28a7c0c6bb0
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-07 23:14:01 -0300

    test(server): ensure AccountMongoRepository returns an account on success

---
+++ server/src/infra/database/mongodb/account-repository/account-repository.spec.ts
@@ -0,0 +1,34 @@
+import { MongoHelper } from '../helpers';
+import { AccountMongoRepository } from './account-repository';
+
+describe('AccountMongoRepository', () => {
+  let sut: AccountMongoRepository;
+
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL));
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(() => {
+    sut = new AccountMongoRepository();
+  });
+
+  it('should return an account on success', async () => {
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    });
+
+    expect(account).toBeDefined();
+    expect(account.id).toBeDefined();
+    expect(account).toMatchObject({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    });
+  });
+});
