86721a5843f4df296c7389cd590c45e32e7319dd
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-08 00:19:43 -0300

    feat(server): ensure cors is enabled

--- server/src/main/config/middlewares.ts
+++ server/src/main/config/middlewares.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express';
 
-import { bodyParser } from '../middlewares';
+import { bodyParser, cors } from '../middlewares';
 
 export function setupMiddlewares(app: Express): void {
   app.use(bodyParser);
+  app.use(cors);
 }
