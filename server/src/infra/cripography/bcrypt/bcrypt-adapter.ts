e849d6dc87f0d62c8461f7a7f184ab738def7b12
Harlan Cleiton <harlancleiton@gmail.com>
2021-03-07 21:14:21 -0300

    feat(server): ensure BCryptAdapter return a hash on success

--- server/src/infra/cripography/bcrypt/bcrypt-adapter.ts
+++ server/src/infra/cripography/bcrypt/bcrypt-adapter.ts
@@ -5,8 +5,8 @@ import { Hash } from '~/data/protocols';
 export class BCryptAdapter implements Hash {
   async make(payload: string): Promise<string> {
     const salt = await bcrypt.genSalt(12);
-    await bcrypt.hash(payload, salt);
+    const hash = await bcrypt.hash(payload, salt);
 
-    return '';
+    return hash;
   }
 }
